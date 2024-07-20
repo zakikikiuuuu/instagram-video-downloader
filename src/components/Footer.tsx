665dae03ca36a0261d54cc572b7ba53fcfd8baf0
zakikikiuuuu <zakidpedia@gmail.com>
2024-07-20 12:27:50 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,20 @@
 import Link from "next/link";
+import { useEffect } from "react";
 
 export function Footer() {
+  useEffect(() => {
+    const yearElement = document.getElementById('year');
+    if (yearElement) {
+      yearElement.textContent = new Date().getFullYear();
+    }
+  }, []);
+
   return (
     <footer className="text-secondary-foreground bg-background/50 h-8 w-full text-center">
-      <div className="border-input flex items-center justify-between border-t px-4 py-2">
-        <div aria-label="Disclaimer" className="text-sm font-semibold">
-          We are not affiliated with Instagram or Meta
-        </div>
-        <div className="flex items-center gap-1 text-sm">
-          <Link href="/terms-of-service" className="hover:underline">
-            Terms of Service
-          </Link>
-          <span aria-hidden="true" className="mx-2 select-none">
-            |
-          </span>
-          <Link href="/privacy-policy" className="hover:underline">
-            Privacy Policy
-          </Link>
-        </div>
+      <div className="border-input flex items-center justify-center border-t px-4 py-2">
+        <h3 className="text-sm font-semibold">
+          Made by <a href="https://instagram.com/adzzakiii" className="hover:underline">Zaki</a> | &copy; <span id="year"></span> All Rights Reserved
+        </h3>
       </div>
     </footer>
   );
